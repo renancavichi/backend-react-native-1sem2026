7285980d6f5e0f6cdb4a6cbd5662adc2aafd6c47
Renan Cavichi <renancavichi@gmail.com>
2026-03-23 21:12:14 -0300

    add list user

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import { prisma } from './helper/prismaClient.js'
 
 const app = express()
-const PORT = 3000
+const PORT = 3333
 
 app.use(express.json())
 app.use(cors())
@@ -26,7 +26,20 @@ app.post('/user', async (req, res) => {
     return res.status(400).json({message: 'Erro ao criar usuário'})
   
   return res.json({message: 'Usuário criado com sucesso', user: result})
-})  
+})
+
+app.get('/user', async (req, res) => {
+  let result
+
+  try {
+    result = await prisma.user.findMany()
+  } catch (error) {
+    console.error('Error fetching users:', error)
+    return res.status(500).json({message: 'Erro ao buscar usuários'})
+  }
+
+  return res.json({users: result})
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running http://localhost:${PORT}`)
